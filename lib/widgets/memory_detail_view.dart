a1cc2119d7ac679608aa14af8ccf9b9db02785ac
unknown <efegrkm1@gmail.com>
2026-02-14 23:22:49 +0300

    fix

--- lib/widgets/memory_detail_view.dart
+++ lib/widgets/memory_detail_view.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:image_picker/image_picker.dart';
